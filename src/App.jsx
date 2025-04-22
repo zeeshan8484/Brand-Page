a4b0ab10f12d830d2e91d7f24f47a1dd40f25071
Muhammad Zeeshan Akram <akramzeeshan837@gmail.com>
2025-04-22 17:08:32 +0500

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigation from "./components/Navigation";
+import "./App.css";
+import HeroSection from "./components/HeroSection";
+function App() {
+  return (
+    <>
+      <Navigation />
+      <HeroSection />
+    </>
+  );
+}
+
+export default App;
